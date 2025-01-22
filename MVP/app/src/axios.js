fb57a8daa5c864d3933bc5b25f1b48ab844311e6
root <root@docxtalkserveur.ltd>
2025-01-22 17:01:43 +0000

    ajout des configuration pour laravel sanctum

--- MVP/app/src/axios.js
+++ MVP/app/src/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-	baseURL: "https://www.docx-talk.com/api",
+	baseURL: "https://docxtalks.com/api",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "multipart/form-data",
